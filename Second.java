b762156d2508dee5b852b022849e92502997b54c
AVI111317 <islam3z@yahoo.com>
2023-02-06 18:54:52 -0500

    add new project

---
+++ Second.java
@@ -0,0 +1,29 @@
+package com;
+
+public class Second {
+public void home() {
+	<dependencies>
+	<!-- https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java -->
+	<dependency>
+	    <groupId>org.seleniumhq.selenium</groupId>
+	    <artifactId>selenium-java</artifactId>
+	    <version>4.8.0</version>
+	</dependency>
+	<!-- https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api -->
+	<dependency>
+	    <groupId>junit</groupId>
+	    <artifactId>junit</artifactId>
+	    <version>4.13.2</version>
+	    
+	</dependency>
+	<!-- https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java -->
+	<dependency>
+	    <groupId>org.seleniumhq.selenium</groupId>
+	    <artifactId>selenium-java</artifactId>
+	    <version>4.8.0</version>
+	</dependency>
+
+	</dependencies>
+	</project>
+}
+}
